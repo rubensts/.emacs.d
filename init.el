3d4643cc25db7f1b1dd231840b044add563bd090
Rubens Souza <rubens.souza@2ndquadrant.it>
2017-11-01 11:34:16 +0100

    Adding Cask as package manager

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;;; Init --- Summary
+;;; init.el --- Emacs configuration of Rubens Souza -*- lexical-binding: t; -*-
 ;;; Commentary:
 ;;; Code:
 
@@ -9,48 +9,76 @@
 ;; You may delete these explanatory comments.
 ;;(package-initialize)
 
-(setq gc-cons-threshold most-positive-fixnum)
-(add-hook 'after-init-hook #'(lambda ()
-                               ;; restore after startup
-                               (setq gc-cons-threshold 800000)))
+(add-to-list 'load-path (expand-file-name
+                         "lisp" user-emacs-directory))
 
-(require 'cask "/usr/local/share/emacs/site-lisp/cask/cask.el")
-(cask-initialize)
-(require 'pallet)
-(pallet-mode t)
+;;----------------------------------------------------------------------------
+;; Temporarily reduce garbage collection during startup
+;; https://github.com/purcell/emacs.d/blob/master/init.el
+;;----------------------------------------------------------------------------
+(defconst my/initial-gc-cons-threshold gc-cons-threshold
+  "Initial value of `gc-cons-threshold' at start-up time.")
+(setq gc-cons-threshold (* 128 1024 1024))
+(add-hook 'after-init-hook
+          (lambda ()
+            (setq gc-cons-threshold my/initial-gc-cons-threshold)))
 
-(require 's)
-(require 'f)
-(require 'ht)
-(require 'git)
-(require 'ert)
-(require 'use-package)
+(let ((file-name-handler-alist nil))
 
-(setq default-directory (f-full (getenv "HOME")))
+  ;;----------------------------------------------------------------------------
+  ;; Load Cask - Package installation/dependencies are handled by cask & pallet
+  ;;----------------------------------------------------------------------------
+  (require 'cask "/usr/local/share/emacs/site-lisp/cask/cask.el")
+  (cask-initialize)
+  (require 'pallet)
+  (pallet-mode t)
 
-(defun load-local (file)
-  (load (f-expand file user-emacs-directory)))
+  ;;----------------------------------------------------------------------------
+  ;; Set some default options
+  ;;----------------------------------------------------------------------------
+  (setq-default
+   message-log-max 10000           ; increase size of *Messages* buffer
+   load-prefer-newer t             ; avoid using outdated compiled files
+   inhibit-default-init t          ; don't call default.el (default settings) after init.el
+   package-enable-at-startup nil)  ; don't call package-initialize after init.el
 
-(use-package org
-  :demand t)
+  ;;----------------------------------------------------------------------------
+  ;; Require use-package - loading packages is handled by it
+  ;;----------------------------------------------------------------------------
+  (require 'use-package)
+  (setq-default use-package-always-defer t)
 
-(use-package exec-path-from-shell
-  :init
-  (exec-path-from-shell-initialize))
+  ;;----------------------------------------------------------------------------
+  ;; Load required packages
+  ;;----------------------------------------------------------------------------
+  (use-package s)  ; required library
+  (use-package f)  ; required library
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (avy pallet))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+  (use-package delight)    ; to use :delight with use-package
+  (use-package bind-key)   ; to use :bind (and variants) with use-package
+  (use-package no-littering :demand t)   ; keeps ~/.emacs.d clean
+  (use-package validate :demand t)
+  (use-package org)
 
+  ;;----------------------------------------------------------------------------
+  ;; Load (tangle) rest of the configuration through org - Literate Programming
+  ;;
+  ;; org-babel-load-file increases startup time, so only do it if necessary.
+  ;; To reload any config changes, delete emacs-init.el and restart emacs.
+  ;;----------------------------------------------------------------------------
+  (if (file-exists-p (expand-file-name "emacs-init.el" user-emacs-directory))
+      (load-file (expand-file-name "emacs-init.el" user-emacs-directory))
+    ;; else
+    (org-babel-load-file (expand-file-name "readme.org" user-emacs-directory)))
 
-(provide 'init)
+  ;;----------------------------------------------------------------------------
+  ;; Initialize Emacs server - allow access from emacsclient
+  ;;----------------------------------------------------------------------------
+  (use-package server
+    :demand t
+    :config
+    (unless (server-running-p)
+      (server-start)))
+
+  (provide 'init))
 ;;; init.el ends here
